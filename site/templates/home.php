107caf22f70501426c044c289a6c4b61a3c4494f
Sarah Garcin <contact@sarahgarcin.com>
2020-08-24 14:56:04 +0200

    clean website theme

--- site/templates/home.php
+++ site/templates/home.php
@@ -5,23 +5,24 @@
 		<section class="introduction col-xs-12 col-md-4">
 			<?= $page->text()->kt()?>
 		</section>
-		<section class="news col-xs-12 col-md-8 row">
-			<?php foreach($page->news()->toStructure() as $news):?>
-				<article class="col-xs-12 col-md-6">
-					<div class="featured-img-wrapper">
-						<?php $cover = $news->cover()->toFile()?>
-						<div class="featured-img" style="background-image: url('<?=$cover->url()?>')"></div>
-						<div class="featured-img background"></div>
-					</div>
-					<div class="news-text">
-						<?= $news->text()->kt()?>
-					</div>
-				</article>
-			<?php endforeach;?>
-		</section>
-<!-- 		<section class="gallery col-md-10 col-md-offset-1">
-			<?php //snippet('projects-list') ?>
-		</section> -->
+		<?php if($page->news()->isNotEmpty()):?>
+			<section class="news col-xs-12 col-md-8 row">
+				<?php foreach($page->news()->toStructure() as $news):?>
+					<article class="col-xs-12 col-md-6">
+						<?php if($news->cover()->isNotEmpty()):?>
+							<div class="featured-img-wrapper">
+								<?php $cover = $news->cover()->toFile()?>
+								<div class="featured-img" style="background-image: url('<?=$cover->url()?>')"></div>
+								<div class="featured-img background"></div>
+							</div>
+						<?php endif;?>
+						<div class="news-text">
+							<?= $news->text()->kt()?>
+						</div>
+					</article>
+				<?php endforeach;?>
+			</section>
+		<?php endif;?>
 
 	</main>
 
